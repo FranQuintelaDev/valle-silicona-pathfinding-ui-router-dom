f7389349c98bc6dc136d828320584bfccaafe19e
FranQuintelaVela <fraquivel@gmail.com>
2022-11-28 07:38:27 +0100

    Cleaned project

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,25 +1,10 @@
-import { Counter } from "./components/Counter";
-import { FetchData } from "./components/FetchData";
-import { Home } from "./components/Home";
 import { Grid } from "./components/Grid";
 
 const AppRoutes = [
   {
     index: true,
-    element: <Home />
-  },
-  {
-    path: '/counter',
-    element: <Counter />
-  },
-  {
-    path: '/fetch-data',
-    element: <FetchData />
-  },
-  {
-    path: '/grid',
     element: <Grid />
-  },
+  }
 ];
 
 export default AppRoutes;
